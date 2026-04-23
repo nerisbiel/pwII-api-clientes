14c1b973ee284ef020c224a300a3aa362544d2c4
wagnermarques <wagnerdocri@gmail.com>
2026-04-22 22:44:14 -0300

    aula 2204

---
+++ scr/model/Cliente.js
@@ -0,0 +1,10 @@
+class Cliente{
+    constructor(id, nome, telefone, endereco){
+        this.nome = nome;
+        this.telefone = telefone;
+        this.endereco = endereco;
+        this.id = id
+    }
+}
+
+module.exports = Cliente
